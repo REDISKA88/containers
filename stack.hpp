4c129890468a660ce58ab6a380f1b79a1a839e48
REDISKA88 <virusloki@ya.ru>
2022-01-22 19:19:54 +0300

    start code stack: add some methods

--- stack.hpp
+++ stack.hpp
@@ -1,20 +1,50 @@
 #ifndef STACK_H
 #include <vector>
-// here we will replace <std::vector> to "ft_vector"
-template <typename T, class Container = std::vector<T> >
-class Stack : public std::vector<T> {
+
+namespace ft {
+    template<typename T, class Container = std::vector<T> >
+    class stack;
+}
+template<typename T, class Container>
+class ft::stack {
 
 public:
     typedef T value_type;
     typedef Container container_type;
     typedef std::size_t size_type;
 private:
-    container_type  _ctnr;
+    container_type _ct;
+
 public:
-    explicit Stack(const container_type& _ctnr){}
+
+    stack() : _ct() {}
+
+    explicit stack(const container_type &_ct = container_type()) {
+        this->_ct = _ct;
+    }
 
 
+
+    //METHODS
+    bool empty() const {
+            return _ct.empty();
+    }
+
+    std::size_t size() const {
+        return _ct.size();
+    }
+
+    void pop() {
+        _ct.pop_back();
+    }
+
+    void push(value_type &v) {
+        _ct.push_back(v);
+    }
+
+    ~stack() {};
 };
+
 
 #define STACK_H
 #endif
